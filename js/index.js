76cf0f563506c5c46d78b5a01803e5c12f75ae5c
sergey <seslik627@gmail.com>
2023-03-09 11:20:07 +0200

    Цыклы

--- js/index.js
+++ js/index.js
@@ -161,11 +161,10 @@ if (sergey[1] <= 18 && rita[1] >= 13) {
 "use strict";
 
 let num = 50;
-
-/* while (num <= 55) {
+ while (num <= 55) {
   console.log(num);
   num++;
-} */
+} 
 
 /* do {
   console.log(num);
@@ -219,4 +218,112 @@ first:for (let i = 0; i < 3; i++) {
         console.log(`Third level: ${k}`);
       }
     }
+}
+
+let text = "";
+const num = 10;
+for(let i = 1; i < num; i++) {
+  for (let j = 0; j < i; j++) {
+    text += "Виталик не балуйся ";
+  }
+  text += "\n";
+}
+console.log(text);
+
+let vitalik = "";
+const number = 9;
+for (let i = 1; i < number; i++) {
+  for (let j = 0; j < i; j++) {
+    vitalik += "Виталик молодец ";
+  }
+  vitalik += "\n";
+}
+console.log(vitalik);
+
+for(let i = 5; i <= 10; i++) {
+  console.log(i);
+}
+for(let i = 20; i >= 10; i--) {
+  if (i === 13) {
+    break;
+  }
+  console.log(i);
+} 
+//Место для первой задачи 
+for (let i = 5; i <= 10; i++) {
+  console.log(i);
+}
+//Место для 2 задачи
+for (let i = 20; i >= 10; i--) {
+  if (i === 13) {
+    break;
+  }
+  console.log(i);
+}
+//Место для 3 задачи
+for (let i = 2; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+//Место для 4 задачи
+//Перевести for на while
+//Цикл, который нужно переписать:
+ 
+ for (let i = 2; i <= 16; i++) {
+     if (i % 2 === 0) {
+         continue;
+     } else {
+         console.log(i);
+     }
+ }
+ //Цикл While
+ let i = 2;
+ while (i < 16) {
+  i++;
+  if( i % 2 === 0) {
+    continue;
+  } else {
+    console.log(i);
+  }
+} 
+//Место для 5 задачи
+const mass = [];
+let no = 5;
+
+function fifthTask() {
+  const arrayOfNumbers = [];
+  let inputNum = 5;
+  // Пишем решение вот тут
+  for (let i = 0; i <= 5; i++) {
+      arrayOfNumbers[i] = inputNum;
+      inputNum++;
+  }
+  console.log(arrayOfNumbers);
+  // Не трогаем
+  return arrayOfNumbers;
+}
+
+// Массив
+let numberOf = [2, "lol", 34];
+console.log(numberOf[0]);
+
+// Добавить в масив элементы через цыкл 
+let kik = [];
+let bad = 10;
+for (let i = 5; i <= bad; i++) {
+  kik[i] <= bad;
+  bad++;
+}
+console.log(kik);
+
+function fifthTask() {
+  const arrayOfNumbers = [];
+
+  for (let i = 5; i < 11; i++) {
+      arrayOfNumbers[i - 5] = i;
+  }
+
+  console.log(arrayOfNumbers);
+  return arrayOfNumbers;
 }
